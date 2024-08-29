4b6210fbee858cdfe1e5fc9c9e05ad5de4d030b0
Daniil Glushakov <anydnny@gmail.com>
2024-08-29 17:41:33 +0300

    Исправлены тесты cypress

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -5,8 +5,13 @@ import * as orderInfo from '../fixtures/orderResponse.json';
 const BUN = `[data-cy=${'643d69a5c3f7b9001cfa093c'}]`;
 const MAIN_INGREDIENT = `[data-cy=${'643d69a5c3f7b9001cfa0949'}]`;
 const MODAL = '[data-cy="modal"]';
-const MODAL_TITLE = '[data-cy="modalIngredientTitle"]';
 const MODAL_CLOSE = '[data-cy="modalCloseButton"]';
+const MODAL_TITLE = '[data-cy="modalIngredientTitle"]';
+const MODAL_IMAGE = '[data-cy="modalIngredientImage"]';
+const MODAL_CALORIES = '[data-cy="modalIngredientCalories"]';
+const MODAL_PROTEINS = '[data-cy="modalIngredienProteins"]';
+const MODAL_FAT = '[data-cy="modalIngredientFat"]';
+const MODAL_CARB = '[data-cy="modalIngredienCarb"]';
 const BURGERCONSTRUCTOR = '[data-cy="constructor"]';
 
 let fetchIngredientsResult: { success: boolean; data: TIngredient[] };
@@ -43,9 +48,27 @@ describe('Проверка наличия ингредиентов на стра
 
 describe('Проверка модального окна ингредиента', () => {
   it('Открытие модалки', () => {
+    cy.get(MODAL).should('not.exist');
     cy.get(MAIN_INGREDIENT).click();
     cy.get(MODAL).should('exist');
+  });
+  it('Проверка контента модалки', () => {
+    cy.get(MAIN_INGREDIENT).click();
     cy.get(MODAL_TITLE).should('contain', fetchIngredientsResult.data[1].name);
+    cy.get(MODAL_IMAGE);
+    cy.get(MODAL_CALORIES).should(
+      'contain',
+      fetchIngredientsResult.data[1].calories
+    );
+    cy.get(MODAL_PROTEINS).should(
+      'contain',
+      fetchIngredientsResult.data[1].proteins
+    );
+    cy.get(MODAL_CARB).should(
+      'contain',
+      fetchIngredientsResult.data[1].carbohydrates
+    );
+    cy.get(MODAL_FAT).should('contain', fetchIngredientsResult.data[1].fat);
   });
   it('Закрытие модалки по клику', () => {
     cy.get(MAIN_INGREDIENT).click();
@@ -59,6 +82,19 @@ describe('Создание заказа', () => {
   beforeEach(() => {
     localStorage.setItem('refreshToken', userInfo.refreshToken);
     cy.setCookie('accessToken', userInfo.accessToken);
+    // Проверка что в конструкторе пусто
+    cy.get(BURGERCONSTRUCTOR).should(
+      'not.contain',
+      `${fetchIngredientsResult.data[0].name} (верх)`
+    );
+    cy.get(BURGERCONSTRUCTOR).should(
+      'not.contain',
+      `${fetchIngredientsResult.data[0].name} (низ)`
+    );
+    cy.get(BURGERCONSTRUCTOR).should(
+      'not.contain',
+      `${fetchIngredientsResult.data[1].name}`
+    );
     // Добавление ингредиентов
     cy.get(BUN).children('button').click();
     cy.get(MAIN_INGREDIENT).children('button').click();
@@ -85,6 +121,19 @@ describe('Создание заказа', () => {
     );
   });
   it('Оформление заказа и проверка модалки', () => {
+    // Проверка что ингредиенты добавились в конструктор
+    cy.get(BURGERCONSTRUCTOR).should(
+      'exist',
+      `${fetchIngredientsResult.data[0].name} (верх)`
+    );
+    cy.get(BURGERCONSTRUCTOR).should(
+      'exist',
+      `${fetchIngredientsResult.data[0].name} (низ)`
+    );
+    cy.get(BURGERCONSTRUCTOR).should(
+      'exist',
+      `${fetchIngredientsResult.data[1].name}`
+    );
     cy.get(BURGERCONSTRUCTOR).contains('Оформить заказ').click();
     cy.get(MODAL).contains(`${orderInfo.order.number}`);
     cy.get(MODAL_CLOSE).click();
